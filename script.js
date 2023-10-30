70c0f79a28ed047bcb78ffa5f26ba9abef93bed1
Raam <raam.sanghani@gmail.com>
2023-10-30 15:56:55 -0400

    Added teh rows to the createGrid function

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const grid = document.querySelector(".Grid");
 const row = document.querySelector(".Row");
 
-const square = document.createElement("div");
-square.classList.add("square");
+// const square = document.createElement("div");
+// square.classList.add("square");
 
 function createGrid(size) {
     for (let i = 0; i < size; i++){
@@ -10,8 +10,13 @@ function createGrid(size) {
         square.classList.add("square");
         row.appendChild(square);
     };
+    for (let j = 0; j < size - 1; j++){
+        const clone = row.cloneNode(true);
+        grid.appendChild(clone);
+    }
+
 };
 
 
-// createGrid(16);
+createGrid(16);
 // row.appendChild(square);
