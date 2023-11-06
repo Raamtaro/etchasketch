fe7ba0d9bf3ec62a666dbd6eb200bf98dcd90dd1
Raam <raam.sanghani@gmail.com>
2023-11-06 14:17:02 -0500

    the last git commit message was poorly written. But updating having deleted the comments from last commit

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const grid = document.querySelector(".Grid");
 const row = document.querySelector(".Row");
 
-// const square = document.createElement("div");
-// square.classList.add("square");
 
 function createGrid(size) {
-    // row.style.height = (grid.clientHeight/size).toString();
+
     
     for (let i = 0; i < size; i++){
         let square = document.createElement("div");
@@ -13,9 +11,6 @@ function createGrid(size) {
 
         square.style.height = (grid.clientHeight/size).toString() + 'px';
         square.style.width = (grid.clientWidth/size).toString() + 'px';
-
-        // console.log(typeof(square.style.height));
-        // console.log(square.style.width);
 
         row.appendChild(square);
     };
@@ -29,8 +24,3 @@ function createGrid(size) {
 
 
 createGrid(17);
-// console.log(typeof((grid.clientHeight/16).toString() + 'px'))
-// console.log((grid.clientHeight/16).toString() + 'px');
-
-
-// row.appendChild(square);
